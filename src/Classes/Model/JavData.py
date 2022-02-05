db095b1097958b0d1a6d221efa95350377e458ee
1780089183@qq.com <mvpprime980805>
2022-02-06 01:02:49 +0800

    2201205-2 dev

--- src/Classes/Model/JavData.py
+++ src/Classes/Model/JavData.py
@@ -4,40 +4,106 @@ from Classes.Enums import CompletionStatusEnum, CutTypeEnum
 
 
 class JavData(object):
+    """
+    一部影片的元数据
+
+    在整理流程中逐渐从各大网站充实数据，最终以json格式写到本地
+    """
+
     def __init__(self, **entries):
-        self.Car = ''                # 1 车牌
-        self.CarOrigin = ''           # 2 原始车牌
-        self.Series = ''              # 3 系列
-        self.Title = ''               # 4 原标题
-        self.TitleZh = ''             # 5 简体中文标题
-        self.Plot = ''                # 6 剧情概述
-        self.PlotZh = ''              # 7 简体剧情
-        self.Review = ''              # 8 剧评
-        self.Release = '1970-01-01'   # 9 发行日期
-        self.Runtime = 0              # 10 时长
-        self.Director = ''            # 11 导演
-        self.Studio = ''              # 12 制造商
-        self.Publisher = ''           # 13 发行商
-        self.Score = 0                # 14 评分
-        self.CoverLibrary = ''        # 15 封面Library
-        self.CoverBus = ''            # 16 封面Bus
-        self.CutType = CutTypeEnum.left.value    # 17 裁剪方式
-        self.Javdb = ''               # 18 db编号
-        self.Javlibrary = ''          # 19 library编号
-        self.Javbus = ''              # 20 bus编号
-        self.Arzon = ''               # 21 arzon编号
-        self.CompletionStatus = CompletionStatusEnum.unknown.value    # 22 完成度，三大网站为全部
-        self.Version = 1              # 23 版本
-        self.Genres = []              # 24 类型
-        self.Actors = []              # 25 演员们
+        self.Car = ''
+        """1 车牌"""
+
+        self.CarOrigin = ''
+        """2 原始车牌"""
+
+        self.Series = ''
+        """3 系列"""
+
+        self.Title = ''
+        """4 原标题"""
+
+        self.TitleZh = ''
+        """5 简体中文标题"""
+
+        self.Plot = ''
+        """6 剧情概述"""
+
+        self.PlotZh = ''
+        """7 简体剧情"""
+
+        self.Review = ''
+        """8 剧评"""
+
+        self.Release = '1970-01-01'
+        """9 发行日期"""
+
+        self.Runtime = 0
+        """10 时长"""
+
+        self.Director = ''
+        """11 导演"""
+
+        self.Studio = ''
+        """12 制造商"""
+
+        self.Publisher = ''
+        """13 发行商"""
+
+        self.Score = 0
+        """14 评分"""
+
+        self.CoverLibrary = ''
+        """15 封面Library"""
+
+        self.CoverLibrary = ''
+        """15 封面Library"""
+
+        self.CoverBus = ''
+        """16 封面Bus"""
+
+        self.CutType = CutTypeEnum.left.value
+        """17 裁剪方式"""
+
+        self.Javdb = ''
+        """18 db编号"""
+
+        self.Javlibrary = ''
+        """19 library编号"""
+
+        self.Javbus = ''
+        """20 bus编号"""
+
+        self.Arzon = ''
+        """21 arzon编号"""
+
+        self.CompletionStatus = CompletionStatusEnum.unknown.value
+        """22 完成度，三大网站为全部"""
+
+        self.Version = 1
+        """23 版本"""
+
+        self.Genres = []
+        """24 类型"""
+
+        self.Actors = []
+        """25 演员们"""
+
         self.Modify = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        """26 修改时间"""
+
         self.__dict__.update(entries)
 
     def prefect_completion_status(self):
+        """
+        更新一下整理的完成度
+
+        整理流程的末尾，
+        """
         if self.Javdb:
             if self.Javlibrary:
                 if self.Javbus:
-                    completion = CompletionStatusEnum.db_library_bus.value
+                    completion = CompletionStatusEnum.db_library_bus.value  # 三个网站全部收集整理
                 else:
                     completion = CompletionStatusEnum.db_library.value
             else:
